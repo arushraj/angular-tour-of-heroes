25f569e63875075b02e560adf6c090c5a10a52cd
arushraj <arushraj7@gmail.com>
2018-04-07 02:18:42 +0530

    angular 2 POC
    Today I have done in angular with Module, component, HTML, CSS, Routing, Service.

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from '../metadata/hero';
+// import { HEROES } from '../metadata/mock-heroes';
+import { HeroService } from '../service/hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  // hero: Hero = { id: 1, name: 'Windstorm' };
+  selectedHero: Hero;
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  // getHeroes(): void {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+}
